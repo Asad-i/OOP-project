85a154b39df5da8339c2425ccf9d44f0d6528905
GalvinHuang <140358290+GalvinHuang@users.noreply.github.com>
2023-10-17 23:48:50 +1030

    Add files via upload

--- MagicalAttack.cpp
+++ MagicalAttack.cpp
@@ -1,16 +1,23 @@
 #include "MagicalAttack.h"
 
-int MagicalAttack::damage_calc() {
-  if ((rand() & 100) < accuracy) {
+MagicalAttack::MagicalAttack() { 
+ attack_type = type;
+ power = 50;
+}
+int MagicalAttack::damage_calc(){
+  if ((rand() & 100) < accuracy){
     // Calculate the damage using the formula
-    damage = ((2 * level) / 5 + 2) * power * (m_attack / opponent_m_defence) / 50 + 2;
-    int consecutive = 0; 
+    damage = ((2 * level) / 5 + 2) * power*(m_attack / opponent_m_defence) / 50 + 2;
+    if (attack_type == type){
+      damage * 1.5;
+    }
+    int consecutive = 0;
   } else {
     cout << "Move Missed!" << endl;
   }
+  return damage;
 }
-
-void MagicalAttack::set_opponent_m_defence(int para_m_defence) {
-  opponent_m_defence = para_m_defence;
-  int consecutive = 0; 
+void MagicalAttack::set_opponent_m_defence(int para_p_defence){
+  opponent_m_defence = para_p_defence;
+  int consecutive = 0;
 }
--- PhysicalAttack.cpp
+++ PhysicalAttack.cpp
@@ -1,17 +1,22 @@
 #include "PhysicalAttack.h"
 
-int PhysicalAttack::damage_calc() {
-  if ((rand() & 100) < accuracy) {
+PhysicalAttack::PhysicalAttack(){
+ attack_type = type;
+ power = 50;
+}
+int PhysicalAttack::damage_calc(){
+  if ((rand() & 100) < accuracy){
     // Calculate the damage using the formula
-    damage =
-        ((2 * level) / 5 + 2) * power * (p_attack / opponent_p_defence) / 50 +
-        2;
+    damage = ((2 * level) / 5 + 2) * power*(p_attack / opponent_p_defence) / 50 + 2;
+    if (attack_type == type){
+      damage * 1.5;
+    }
     int consecutive = 0;
   } else {
     cout << "Move Missed!" << endl;
   }
+  return damage;
 }
-
-void PhysicalAttack::set_opponent_p_defence(int para_p_defence) {
+void PhysicalAttack::set_opponent_p_defence(int para_p_defence){
   opponent_p_defence = para_p_defence;
 }
