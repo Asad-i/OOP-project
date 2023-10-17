a4bb4aff917862589f71763411ea8c0ba4a0cb54
ArnavKapoor-1813447 <140463259+ArnavKapoor-1813447@users.noreply.github.com>
2023-10-17 22:05:08 +1030

    Add files via upload

--- Character.cpp
+++ Character.cpp
@@ -1,5 +1,6 @@
 #include "Character.h"
 
+// Default constructor
 Character::Character() {
   name = "Character";
   type = "Fire";
@@ -11,6 +12,8 @@ Character::Character() {
   m_defence = 100;
   speed = 100;
 }
+
+// Parameterized constructor
 Character::Character(string para_name, string para_type, int para_level, int para_health, int para_p_attack, int para_m_attack, int para_p_defence, int para_m_defence, int para_speed) {
   name = para_name;
   type = para_type;
@@ -22,12 +25,15 @@ Character::Character(string para_name, string para_type, int para_level, int par
   m_defence = para_m_defence;
   speed = para_speed;
 }
+
+// Function to set the character's name
 void Character::set_name() {
-  cout << "New Name: ";
-  string stringInput;
-  cin >> stringInput;
-  name = stringInput;
+    cout << "New Name: ";
+    cin.ignore(); // Clear the input buffer, if needed.
+    getline(cin, name); // Read the entire line as the name.
 }
+
+// Function to set the character's type
 void Character::set_type() { 
   cout << "Select 1 For Fire" << endl;
   cout << "Select 2 For Grass" << endl;
@@ -40,32 +46,38 @@ void Character::set_type() {
   }
   if (integerInput == 1){
     type = "Fire";
-    cout << "Sucessfully changed type to Fire." << endl;
+    cout << "Successfully changed type to Fire." << endl;
   } else if (integerInput == 2) {
     type = "Grass";
-    cout << "Sucessfully changed type to Grass." << endl;
+    cout << "Successfully changed type to Grass." << endl;
   } else if (integerInput == 3) {
     type = "Water";
-    cout << "Sucessfully changed type to Water." << endl;
+    cout << "Successfully changed type to Water." << endl;
   } else {
-    cout << "Invalid Input. Input must be 1,2 or 3." << endl;
+    cout << "Invalid Input. Input must be 1, 2, or 3." << endl;
   }
 }
+
+// Functions to set various character attributes
 void Character::set_level(int para_level) { level = para_level; }
-void Character::set_health(int para_health) {health = para_health;}
+void Character::set_health(int para_health) { health = para_health; }
 void Character::set_p_attack(int para_p_attack) { p_attack = para_p_attack; }
 void Character::set_m_attack(int para_m_attack) { m_attack = para_m_attack; }
-void Character::set_p_defence(int para_p_defence) {p_defence = para_p_defence;}
-void Character::set_m_defence(int para_m_defence) {m_defence = para_m_defence;}
+void Character::set_p_defence(int para_p_defence) { p_defence = para_p_defence; }
+void Character::set_m_defence(int para_m_defence) { m_defence = para_m_defence; }
 void Character::set_speed(int para_speed) { speed = para_speed; }
+
+// Function to set character's stats based on given parameters
 void Character::set_stats(int para_level, int para_health, int para_p_attack, int para_m_attack, int para_p_defence, int para_m_defence, int para_speed){
   health = (((2 * para_health * level) / 100) + level + 10);
   p_attack = (((2 * para_p_attack * level) / 100) + 5);
   m_attack = (((2 * para_m_attack * level) / 100) + 5);
   p_defence = (((2 * para_p_defence * level) / 100) + 5);
   m_defence = (((2 * para_m_defence * level) / 100) + 5);
-  speed =(((2 * para_speed * level) / 100) + 5);
+  speed = (((2 * para_speed * level) / 100) + 5);
 }
+
+// Functions to retrieve character attributes
 string Character::get_name() { return name; }
 string Character::get_type() { return type; }
 int Character::get_level() { return level; }
